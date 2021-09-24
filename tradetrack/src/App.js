faf8a05cc396865728f673f56f47f5fecaf63259
Nozulla <mark.repka@charter.net>
2021-09-24 06:59:32 -0500

    Updated

--- tradetrack/src/App.js
+++ tradetrack/src/App.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react'
+import { useEffect, useState } from 'react'
 import Stylesheet from './components/stylesheet'
 
-class App extends Component {
-  render() {
-    return (
-      <div classname="App">
-        <Stylesheet />
-        <h1>Hello People</h1>.
+const stockpriceURL = 'https://yahoo-finance-api.vercel.app/M';
+async function getStocks() {
+  const response = await fetch(stockpriceURL);
+  return response.json();
+}
+
+
+
+function App() {
+  const [price, setPrice] = useState(0);
+
+    useEffect(() => {
+      getStocks()
+        .then((data) => {
+          console.log(data);
+          const theStock = data.chart.result[0];
+          setPrice(theStock.meta.regularMarketPrice.toFixed(2));
+        });        
+    }, []);
+
+    return(
+      <div>
+        <Stylesheet />  
+        <h1>{price}</h1>
       </div>
     );
-  }
 }
+
 
 export default App
